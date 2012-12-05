fead214acd7d60ef5af647e8c503b8b6574a6c8f
Franklin Garcia <fggarcia@gmail.com>
2012-12-05 10:24:18 -0600

    Code import

---
+++ src/com/slideupsample/app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.slideupsample.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.list);
+        String[] items = getResources().getStringArray(R.array.sample_names_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
